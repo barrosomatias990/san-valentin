90a20d07041c11ffe7edbb96cf5399c4538107e7
barrosomatias990 <barrosomatias990@gmail.com>
2026-02-05 17:42:03 -0300

    Proyecto San Valentin

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Feliz San Valentín🤍</h1>
+
+      <img src="/fotos/IMG_0674.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <p>Hermosa de mi vida, gracias por hacerme feliz en cada momento que estamos juntos, sos hermosa🤍</p>
+      <p>Te dejo algunos recuerdos de cuanto nos empezamos a ver</p>
+
+      <img src="/fotos/IMG_0009.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Como olvidarme de las primeras veces que venias a casa, me encantaba verte sonreir y estar conmigo</p>
+
+      <img src="/fotos/IMG_0285.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Para el cumple de mi mami, las cosas estaban empezando a ser lindas de nuevo</p>
+
+      <img src="/fotos/IMG_0292.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Un gran verano, donde disfrutamos monton de cosas lindas</p>
+
+      <img src="/fotos/IMG_1675.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Nuestra primer foto juntos en el gym, un momento que estuve esperando por mucho tiempo. Me encanto</p>
+      
+      <img src="/fotos/IMG_1683.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Una de las ultimas fotos que nos sacamos, me encanta verte feliz, TE AMOOOO AMOR DE MI VIDA</p>
+      
+      <img src="/fotos/IMG_1837.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Esta foto es muy importante, porque fue un rato antes de que te pidiera que fueses mi novia. Amo esta foto</p>
+      
+      <img src="/fotos/IMG-20251219-WA0030.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Hermosa de mi vida, sos todo lo que esta bien. Sos preciosa mi amorrrr</p>
+      
+      <img src="/fotos/IMG_2995.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Fotos random que nos sacamos en tu casa, me encanta estarrrr con vosssss</p>
+      
+      <img src="/fotos/IMG_2373.jpg" style= {{height: "70vh", alignContent: "center", alignItems: "center"}} ></img>
+      <br />
+      <p>Antes de ir a jugar, Gracias por acompañarme siempre, sos el amor de mi vida</p>
+      <br /><br />
+      <p>Gracias por aparecer en mi vida</p>
+      <p>No te das una idea lo feliz que soy con vos</p>
+      <p>Te amo para toda la vida</p>
+      <p>Matias Barroso</p>
+      
+    </div>
+  )
+}
+
+export default App
